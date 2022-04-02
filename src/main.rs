ea044b2a77e329807021c2aa292c62d14709a54f
Lukas Jackowski <l.jackowski@agorate.de>
2022-04-02 18:38:36 +0200

    Fix OscUdpServer

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,11 @@ use bevy::prelude::*;
 use bevy_osc::{OscDispatcher, OscMethod, OscMultiMethod, OscUdpClient, OscUdpServer};
 
 /// Read `OscPacket`s from udp server until no more messages are received and then dispatch them
-fn receive_packets(mut disp: ResMut<OscDispatcher>, mut query: Query<&mut OscUdpServer>, method_query: Query<&mut OscMethod>, multi_method_query: Query<&mut OscMultiMethod>) {
-    let mut osc_udp_server = query.single_mut();
+fn receive_packets(mut disp: ResMut<OscDispatcher>, osc_server: Res<OscUdpServer>, method_query: Query<&mut OscMethod>, multi_method_query: Query<&mut OscMultiMethod>) {
     let mut osc_packets = vec![];
 
     loop {
-        match osc_udp_server.recv() {
+        match osc_server.recv() {
             Ok(o) => match o {
                 Some(p) => osc_packets.push(p),
                 None => break
@@ -24,9 +23,9 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
 
-        .insert_resource(OscUdpServer::new("0.0.0.0:31337"))
+        .insert_resource(OscUdpServer::new("0.0.0.0:31337").unwrap())
         .insert_resource(OscDispatcher::default())
-        .insert_resource(OscUdpClient::new(SocketAddrV4::new(Ipv4Addr::from([1,2,3,4]), 31337).into()))
+        .insert_resource(OscUdpClient::new(SocketAddrV4::new(Ipv4Addr::from([1,2,3,4]), 31337).unwrap()))
         .add_system(receive_packets)
 
         .run();
