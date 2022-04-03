52b3dfbfac1252817717ea2b44737d2d8d7c4277
Lukas Jackowski <l.jackowski@agorate.de>
2022-04-03 22:39:12 +0200

    Update layout

--- src/pyree_modules/mod.rs
+++ src/pyree_modules/mod.rs
@@ -7,6 +7,15 @@ use crate::pyree_modules::randomval::{RandomValBundle};
 
 fn spawn_randomval(mut commands: Commands) {
     commands.spawn_bundle(RandomValBundle::new(0, "test".to_string()));
+    commands.spawn_bundle(RandomValBundle::new(1, "test".to_string()));
+    commands.spawn_bundle(RandomValBundle::new(2, "test".to_string()));
+    commands.spawn_bundle(RandomValBundle::new(3, "test".to_string()));
+    commands.spawn_bundle(RandomValBundle::new(4, "test".to_string()));
+    commands.spawn_bundle(RandomValBundle::new(5, "test".to_string()));
+    commands.spawn_bundle(RandomValBundle::new(6, "test".to_string()));
+    commands.spawn_bundle(RandomValBundle::new(7, "test".to_string()));
+    commands.spawn_bundle(RandomValBundle::new(8, "test".to_string()));
+    commands.spawn_bundle(RandomValBundle::new(9, "test".to_string()));
 }
 
 pub fn pyree_system_set() -> SystemSet {
